0d5d928d151a72b68c09f151089492a38579e4d4
Harrison2301 <harrisonwinters2301@gmail.com>
2019-06-13 20:54:34 +0000

    implemented dynamic tweets

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,6 +1,75 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
 
+// Test / driver code (temporary). Eventually will get this from the server.
+$(document).ready(function() {
+    renderTweets(data);
+})
+const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": {
+          "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+          "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+          "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+        },
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": {
+          "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+          "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+          "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+        },
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    },
+    {
+      "user": {
+        "name": "Johann von Goethe",
+        "avatars": {
+          "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+          "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+          "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+        },
+        "handle": "@johann49"
+      },
+      "content": {
+        "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+      },
+      "created_at": 1461113796368
+    }
+  ];
+function createTweetElement(Object){
+let profileImgLink = Object.user.avatars.small;
+let nameHeader = Object.user.name;
+let nameHandle = Object.user.handle;
+let messageContent = Object.content.text;
+let createdDate = new Date(Object.created_at);
+let article =$("<article>")
+let header = $("<header>")
+let img = $("<img>").attr("src", profileImgLink)
+let h2 = $("<h2>").text(nameHeader)
+let span = $("<span>").text(nameHandle)
+let p = $("<p>").text(createdDate)
+let footer =$("<footer>");
+let footerText = $("<p>").text(messageContent)
+footer.append(footerText)
+header.append(img).append(h2).append(span)
+article.append(header).append(footer).append(p)
+return article
+}
+function renderTweets(tweets) {
+    for(let i = 0; i < tweets.length; i++){
+        $('.tweets-container').append(createTweetElement(tweets[i]))  
+    }
+}
